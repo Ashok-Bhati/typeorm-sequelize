160bfa3fd32cea431021df625b8c6464a95d8fd6
Ashok Kumar Bhati <ashok.bhati.tech@hotmail.com>
2025-08-08 20:25:54 +0530

    Refactor ScalarSelectorValue type in select.ts for improved type consistency
    - Updated ScalarSelectorValue type to remove the generic parameter K, enhancing clarity and simplifying type usage in selections.
    - Adjusted SelectJSON type to reflect the change, ensuring consistent handling of scalar selections across the codebase.

--- src/core/types/select.ts
+++ src/core/types/select.ts
@@ -8,10 +8,10 @@ type IsRelationType<T, K extends keyof T> = T[K] extends Array<ObjectLiteral> | 
 type IsScalarType<T, K extends keyof T> = T[K] extends Promise<any> | Date | Array<ObjectLiteral> | ObjectLiteral ? never : K;
 
 export type RelationSelectorValue<T, K extends keyof T> = SelectJSON<RelationField<T, K>>;
-export type ScalarSelectorValue<T, K extends keyof T> = { as: string };
+export type ScalarSelectorValue<T> = { as: string };
 
 export type SelectJSON<T> = {
-  [K in keyof T as IsScalarType<T, K>]?: ScalarSelectorValue<T, K> | boolean;
+  [K in keyof T as IsScalarType<T, K>]?: ScalarSelectorValue<T> | boolean;
 } & {
   [K in keyof T as IsRelationType<T, K>]?: RelationSelectorValue<T, K>;
 };
